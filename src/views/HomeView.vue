d00e549e4a9c9fe41dca0893b57200920cd42a45
airmanxtw <airmanx@stust.edu.tw>
2023-10-19 00:32:18 +0800

    sample

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,25 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { useMonads } from "@/composables/sampleMonads/useMonads";
+const { bmi, bmiThrow } = useMonads();
+
+// sample 1
+try {
+  console.log(bmiThrow(170, 60));
+} catch (error) {
+  console.log(error);
+}
+
+
+// sample 2
+bmi(170, 60).match({
+  ok: (value) => console.log(value),
+  err: (error) => console.log(error),
+});
+
+
+
+
 </script>
 
 <template>
