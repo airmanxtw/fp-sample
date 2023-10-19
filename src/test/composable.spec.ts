8f1caa3fe916c83f439052233087c5af862dd963
airmanxtw <airmanx@stust.edu.tw>
2023-10-20 00:24:19 +0800

    remove evaluate

--- src/test/composable.spec.ts
+++ src/test/composable.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 import { useMonads } from '@/composables/sampleMonads/useMonads'
 describe('test Monads', () => {
-  const { bmi, evaluateCond, evaluate, bmiEvaluate } = useMonads()
+  const { bmi, evaluateCond, bmiEvaluate } = useMonads()
   it('test bmi', () => {
     expect(bmi(173, 45).unwrap()).equal(15.035584215977813)
   })
